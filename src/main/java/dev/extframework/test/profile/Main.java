63a15283c6535c951e22af330ac0b128600ef85a
durganmcbroom <durganmcbroom@gmail.com>
2024-07-09 22:49:29 -0700

    Test exception, bumped version

--- src/main/java/dev/extframework/test/profile/Main.java
+++ src/main/java/dev/extframework/test/profile/Main.java
@@ -6,27 +6,29 @@ import java.lang.management.ManagementFactory;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Simple GUI");
-        JTextArea textArea = new JTextArea(20, 30);
+        throw  new RuntimeException("This is an exception to test if the game started.");
 
-        StringBuilder texts = new StringBuilder();
-        for (String arg : args) {
-            texts.append("  ");
-            texts.append(arg);
-            texts.append("\n");
-        }
-
-        String pid = ManagementFactory.getRuntimeMXBean().getName();
-
-        textArea.append("Process ID: " + pid + "\n");
-        textArea.append("Program arguments:\n");
-        textArea.append(texts.toString());
-
-        JScrollPane scrollPane = new JScrollPane(textArea);
-        frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
-
-        frame.setSize(400, 300);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+//        JFrame frame = new JFrame("Simple GUI");
+//        JTextArea textArea = new JTextArea(20, 30);
+//
+//        StringBuilder texts = new StringBuilder();
+//        for (String arg : args) {
+//            texts.append("  ");
+//            texts.append(arg);
+//            texts.append("\n");
+//        }
+//
+//        String pid = ManagementFactory.getRuntimeMXBean().getName();
+//
+//        textArea.append("Process ID: " + pid + "\n");
+//        textArea.append("Program arguments:\n");
+//        textArea.append(texts.toString());
+//
+//        JScrollPane scrollPane = new JScrollPane(textArea);
+//        frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
+//
+//        frame.setSize(400, 300);
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setVisible(true);
     }
 }
